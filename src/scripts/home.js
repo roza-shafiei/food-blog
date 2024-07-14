f6905cd1d8258cb027bf0483ab604ccacec60a89
roza <roza_shafiei@yahoo.com>
2024-07-14 20:52:57 +0330

    feat(home): add embark on journey pagination and responsive style

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,17 +1,23 @@
 const tagsContainer = document.querySelector('.embark-journey__tags-container');
 const embarkJourneyRecipes = document.querySelector('.embark-journey__recipes');
+const embarkJourneyPagination = document.querySelector('.embark-journey__pagination');
 let tagsLoading = false
 let loading = false;
-window.addEventListener('load', async () => {
+const cardPerPage = 6
+let page = 1
+window.addEventListener('load', () => {
+    getTags()
+})
+
+async function getTags() {
     try {
         tagsLoading = true
         const res = await fetch('https://dummyjson.com/recipes/tags', {method: 'GET'})
         const data = await res.json()
-        let tags = data.slice(0, 8)
+        let tags = data.slice(4, 12).reverse()
         tags.forEach((tag, i) => {
             tagsContainer.insertAdjacentHTML('afterbegin', `<span class="embark-journey__tag" onclick="onSelectingTag(event)">${tag}</span>`)
         })
-
         let embarkJourneyTagList = document.querySelectorAll('.embark-journey__tag');
         embarkJourneyTagList[0]?.classList.add('active-tag')
         getSelectedTagRecipes(embarkJourneyTagList[0].textContent)
@@ -21,7 +27,7 @@ window.addEventListener('load', async () => {
     } finally {
         tagsLoading = false
     }
-})
+}
 
 function onSelectingTag(event) {
     let currentActive = document.querySelector('.active-tag')
@@ -38,29 +44,70 @@ async function getSelectedTagRecipes(name) {
         const res = await fetch(`https://dummyjson.com/recipes/tag/${name}`)
         const data = await res.json()
         let tagRecipesList = data?.recipes
-        console.log(tagRecipesList)
-        if (tagRecipesList.length > 0) {
-            embarkJourneyRecipes.innerHTML = ''
-            tagRecipesList.forEach(recipe => {
-                embarkJourneyRecipes.insertAdjacentHTML('afterbegin', `<div class="food-card">
-                        <div class="food-card__img" style=""></div>
-                        <div class="food-card__content">
-                            <p class="food-card__title">${recipe.name}</p>
-                            <p class="food-card__desc">Indulge in the rich and savory symphony of flavors with our Savory
-                                Herb-Infused Chicken</p>
-                            <div class="food-card__info">
-                                <span>40 Min - easy prep - 3 serves</span>
-                                <button class="secondary-btn btn">view recipe</button>
-                            </div>
-                        </div>
-                    </div>`)
-            })
-        }
-
-
+        generatePagination(tagRecipesList)
     } catch (err) {
         console.log(err)
     } finally {
         loading = false
+    }
+}
+
+function generatePagination(tagRecipesList) {
+    let paginationBoxCount = Math.floor(tagRecipesList.length / cardPerPage)
+    let fragment = document.createDocumentFragment()
+    embarkJourneyPagination.innerHTML = ''
+    for (let index = 0; index < paginationBoxCount; index++) {
+        const span = document.createElement('span')
+        span.classList.add('embark-journey__pagination-box')
+        span.innerText = `${index + 1}`
+        if (page === Number(span.textContent)) {
+            span.classList.add('active-page')
+        }
+        span.addEventListener('click', (event) => {
+            let currentActivePage = document.querySelector('.active-page')
+            page = event.target.textContent
+            currentActivePage.classList.remove('active-page')
+            event.target.classList.add('active-page')
+            createFoodCard(tagRecipesList)
+        })
+        fragment.appendChild(span)
+    }
+    createFoodCard(tagRecipesList)
+    embarkJourneyPagination.append(fragment)
+}
+
+function createFoodCard(tagRecipesList) {
+    let endIndex = page * cardPerPage
+    let startIndex = endIndex - cardPerPage
+    let slicedTagRecipes = tagRecipesList.slice(startIndex, endIndex)
+    if (slicedTagRecipes.length > 0) {
+        embarkJourneyRecipes.innerHTML = ''
+        slicedTagRecipes.forEach(recipe => {
+            const card = document.createElement('div')
+            const cardInfo = document.createElement('div')
+            const imgContainer = document.createElement('div')
+            const cardContent = document.createElement('div')
+            const cardTitle = document.createElement('p')
+            const cardDesc = document.createElement('p')
+            const cardDetails = document.createElement('span')
+            const cardButton = document.createElement('button')
+            card.classList.add('food-card')
+            imgContainer.classList.add('food-card__img')
+            imgContainer.style.background = `url(${recipe.image})`
+            cardContent.classList.add('food-card__content')
+            cardTitle.classList.add('food-card__title')
+            cardDesc.classList.add('food-card__desc')
+            cardTitle.textContent = `${recipe.name}`
+            cardDesc.textContent = `${recipe.description}`
+            cardInfo.classList.add('food-card__info')
+            cardButton.className = 'secondary-btn btn'
+            cardButton.textContent = 'view recipe'
+            cardDetails.textContent = `${recipe.prepTimeMinutes} Min - ${recipe.servings} Serves`
+            cardDetails.classList.add('food-card__info--span')
+            cardContent.append(cardTitle, cardDesc, cardInfo)
+            cardInfo.append(cardDetails, cardButton)
+            card.append(imgContainer, cardContent)
+            embarkJourneyRecipes.appendChild(card)
+        })
     }
 }
