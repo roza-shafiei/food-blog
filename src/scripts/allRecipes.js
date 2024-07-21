74c3fba80100236c6171a53ee9c02055aaf2d874
roza <roza_shafiei@yahoo.com>
2024-07-22 00:48:27 +0330

    clean: remove repetitive functions which already are in generalMethods

--- src/scripts/allRecipes.js
+++ src/scripts/allRecipes.js
@@ -1,3 +1,5 @@
+import {createFoodCard, generatePagination} from "./generalMethods.js";
+
 let cardPerPage = 6
 let page = 1
 let loading = false
@@ -5,7 +7,7 @@ let tagsLoading = false
 const bestRecipesContainer = document.querySelector('.best-recipes__container');
 const tagsContainer = document.querySelector('.tags__container');
 const tagsRecipesPagination = document.querySelector('.pagination');
-const tagsRecipesList = document.querySelector('.tags__recipes');
+const tagsRecipesContainer = document.querySelector('.tags__recipes');
 window.addEventListener('DOMContentLoaded', () => {
     getBestRecipes()
     getTags()
@@ -67,7 +69,8 @@ async function getSelectedTagRecipes(name) {
         const res = await fetch(`https://dummyjson.com/recipes/tag/${name}`)
         const data = await res.json()
         let tagRecipesList = data?.recipes
-        generatePagination(tagRecipesList)
+        generatePagination(tagRecipesList, tagsRecipesPagination, tagsRecipesContainer, cardPerPage, page)
+        createFoodCard(tagRecipesList, tagsRecipesContainer, cardPerPage, page)
     } catch (err) {
         console.log(err)
     } finally {
@@ -75,65 +78,3 @@ async function getSelectedTagRecipes(name) {
     }
 }
 
-function generatePagination(tagRecipesList) {
-    let paginationBoxCount = Math.ceil(tagRecipesList.length / cardPerPage)
-    let fragment = document.createDocumentFragment()
-    tagsRecipesPagination.innerHTML = ''
-    if (paginationBoxCount !== 1) {
-        for (let index = 0; index < paginationBoxCount; index++) {
-            const span = document.createElement('span')
-            span.classList.add('pagination-box')
-            span.innerText = `${index + 1}`
-            if (page === Number(span.textContent)) {
-                span.classList.add('active-page')
-            }
-            span.addEventListener('click', (event) => {
-                let currentActivePage = document.querySelector('.active-page')
-                page = event.target.textContent
-                currentActivePage.classList.remove('active-page')
-                event.target.classList.add('active-page')
-                createFoodCard(tagRecipesList)
-            })
-            fragment.appendChild(span)
-        }
-    }
-    createFoodCard(tagRecipesList)
-    tagsRecipesPagination.append(fragment)
-}
-
-function createFoodCard(tagRecipesList) {
-    let endIndex = page * cardPerPage
-    let startIndex = endIndex - cardPerPage
-    let slicedTagRecipes = tagRecipesList.slice(startIndex, endIndex)
-    if (slicedTagRecipes.length > 0) {
-        tagsRecipesList.innerHTML = ''
-        slicedTagRecipes.forEach(recipe => {
-            const card = document.createElement('div')
-            const cardInfo = document.createElement('div')
-            const imgContainer = document.createElement('div')
-            const cardContent = document.createElement('div')
-            const cardTitle = document.createElement('p')
-            const cardDesc = document.createElement('p')
-            const cardDetails = document.createElement('span')
-            const cardButton = document.createElement('button')
-            card.classList.add('food-card')
-            imgContainer.classList.add('food-card__img')
-            imgContainer.style.background = `url(${recipe.image})`
-            cardContent.classList.add('food-card__content')
-            cardTitle.classList.add('food-card__title')
-            cardDesc.classList.add('food-card__desc')
-            cardTitle.textContent = `${recipe.name}`
-            cardDesc.innerHTML = `<p class="food-card__desc"><span style="font-weight: 500">Ingredients:</span>  ${recipe.ingredients}</p>
-`
-            cardInfo.classList.add('food-card__info')
-            cardButton.className = 'secondary-btn btn'
-            cardButton.textContent = 'view recipe'
-            cardDetails.textContent = `${recipe.prepTimeMinutes} Min - ${recipe.servings} Serves`
-            cardDetails.classList.add('food-card__info--span')
-            cardContent.append(cardTitle, cardDesc, cardInfo)
-            cardInfo.append(cardDetails, cardButton)
-            card.append(imgContainer, cardContent)
-            tagsRecipesList.appendChild(card)
-        })
-    }
-}
