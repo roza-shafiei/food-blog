170fcd52bf55341b20887d178a673b6b4e89241e
roza <roza_shafiei@yahoo.com>
2024-07-22 00:49:23 +0330

    feat: handle active link in desktop navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const navbarMobileIcon = document.querySelector('.navbar-mobile__icon--container');
 const navbarMobileMenu = document.querySelector('.navbar-mobile__menu')
+const desktopNavbarItems = document.querySelectorAll('.navbar__list--link');
+window.addEventListener('load', () => {
+    let selectedPage = window.location.pathname.split('/').pop();
+    desktopNavbarItems.forEach(item => {
+        item.href.includes(selectedPage) && item.classList.add('active-link');
+    })
+
+})
+desktopNavbarItems.forEach(item => {
+    let currentDesktopActiveLink = document.querySelector('.active-link');
+    item.addEventListener('click', (e) => {
+        e.target.classList.toggle('active-link');
+        currentDesktopActiveLink.classList.remove('active-link');
+    })
+})
 let showMobileMenu = false
 navbarMobileIcon.addEventListener('click', (event) => {
     if (!showMobileMenu) {
@@ -15,4 +30,5 @@ navbarMobileIcon.addEventListener('click', (event) => {
 
     }
 })
+
 
