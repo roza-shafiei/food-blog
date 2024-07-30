7606334f63a4d8e62b61ba57164a343d04c354b9
roza <roza_shafiei@yahoo.com>
2024-07-30 21:46:57 +0330

    feat: add loader for recipeId page and make this page responsive

--- src/scripts/recipeId.js
+++ src/scripts/recipeId.js
@@ -12,6 +12,9 @@ const infoTextRate = document.querySelector('.box__list__info--text-rate')
 const infoTextServe = document.querySelector('.box__list__info--text-serve')
 const recipeIdContentInstructions = document.querySelector('.recipe-id__content--instructions')
 const tagsContainer = document.querySelector('.tags-container')
+const recipeIdTagsLoader = document.querySelector('.recipe-id__tags__loader')
+const recipeIdContentLoader = document.querySelector('.recipe-id__content__loader')
+let loading = false
 let tags = null
 const boxListRelated = document.querySelector('.box__list__related')
 
@@ -22,57 +25,64 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 async function getRecipeId() {
-    const res = await fetch(`https://dummyjson.com/recipes/${id}`)
-    const data = await res.json()
-    recipeIdHeaderTitle.textContent = data.name
-    iconsBoxCook.textContent = data.cookTimeMinutes + ' MIN'
-    iconsBoxDifficulty.textContent = data.difficulty + ' PERP'
-    iconsBoxServing.textContent = data.servings + ' SERVES'
-    recipeIdContent.setAttribute('src', `${data.image}`)
-    let ingredientList = data.ingredients
-    if (ingredientList) {
-        ingredientList.forEach(ingredient => {
-            boxListIngredients.insertAdjacentHTML('beforeend', `<li>${ingredient}</li>`)
-        })
-    }
-    infoTextCuisine.textContent = ' ' + data.cuisine
-    infoTextCalories.textContent = ' ' + data.caloriesPerServing
-    infoTextTime.textContent = ' ' + data.cookTimeMinutes
-    infoTextRate.textContent = ' ' + data.rating
-    infoTextServe.textContent = ' ' + data.servings
-    tags = data.tags
-    data.instructions.forEach((instruction, index) => {
-        recipeIdContentInstructions.insertAdjacentHTML('beforeend', `<li>${instruction}</li>`)
-    })
-
-    tags.forEach(tag => {
-        tagsContainer.insertAdjacentHTML('beforeend', `<span class="search-tag">${tag}</span>`)
-    })
-
-    if (tags.length > 0) {
-        boxListRelated.innerHTML = ''
-        const resTag = await fetch(`https://dummyjson.com/recipes/tag/${tags[0]}`)
-        const dataRecipes = await resTag.json()
-        let relatedRecipes = dataRecipes.recipes
-        if (relatedRecipes.length > 0) {
-            setTimeout(() => {
-                // todo: remove the recipe that is opened now
-                // let filteredIndex = relatedRecipes.findIndex(recipe => {
-                //     return recipe.id === Number(id)
-                // })
-                // const splicedArray = relatedRecipes.splice(filteredIndex, 1)
+    try {
+        loading = true
+        recipeIdContentLoader.style.display = 'block'
+        const res = await fetch(`https://dummyjson.com/recipes/${id}`)
+        const data = await res.json()
+        if (data.id) {
+            recipeIdContentLoader.style.display = 'none'
+            recipeIdHeaderTitle.textContent = data.name
+            iconsBoxCook.textContent = data.cookTimeMinutes + ' MIN'
+            iconsBoxDifficulty.textContent = data.difficulty + ' PERP'
+            iconsBoxServing.textContent = data.servings + ' SERVES'
+            recipeIdContent.setAttribute('src', `${data.image}`)
+            let ingredientList = data.ingredients
+            if (ingredientList) {
+                ingredientList.forEach(ingredient => {
+                    boxListIngredients.insertAdjacentHTML('beforeend', `<li>${ingredient}</li>`)
+                })
+            }
+            infoTextCuisine.textContent = ' ' + data.cuisine
+            infoTextCalories.textContent = ' ' + data.caloriesPerServing
+            infoTextTime.textContent = ' ' + data.cookTimeMinutes
+            infoTextRate.textContent = ' ' + data.rating
+            infoTextServe.textContent = ' ' + data.servings
+            tags = data.tags
+            data.instructions.forEach((instruction, index) => {
+                recipeIdContentInstructions.insertAdjacentHTML('beforeend', `<li>${instruction}</li>`)
+            })
+            tags.forEach(tag => {
+                tagsContainer.insertAdjacentHTML('beforeend', `<span class="search-tag">${tag}</span>`)
+            })
+            if (tags.length > 0) {
+                boxListRelated.innerHTML = ''
+                const resTag = await fetch(`https://dummyjson.com/recipes/tag/${tags[0]}`)
+                const dataRecipes = await resTag.json()
+                let relatedRecipes = dataRecipes.recipes
                 if (relatedRecipes.length > 0) {
-                    relatedRecipes.forEach((recipe) => {
-                        boxListRelated.insertAdjacentHTML('beforeend', `<a href="recipeId.html?id=${recipe.id}" class="related__container"><div class="image-container"><img src="${recipe.image}" class="relate__container__image"></div><span class="relate__container__name">${recipe.name}</span></a>`)
+                    setTimeout(() => {
+                        // todo: remove the recipe that is opened now
+                        // let filteredIndex = relatedRecipes.findIndex(recipe => {
+                        //     return recipe.id === Number(id)
+                        // })
+                        // const splicedArray = relatedRecipes.splice(filteredIndex, 1)
+                        if (relatedRecipes.length > 0) {
+                            relatedRecipes.forEach((recipe) => {
+                                boxListRelated.insertAdjacentHTML('beforeend', `<a href="recipeId.html?id=${recipe.id}" class="related__container"><div class="image-container"><img src="${recipe.image}" class="relate__container__image"></div><span class="relate__container__name">${recipe.name}</span></a>`)
 
-                    })
-                } else {
-                    boxListRelated.insertAdjacentHTML('beforeend', `<div class="image-container">Sorry! No Similar Recipe</div>`)
+                            })
+                        } else {
+                            boxListRelated.insertAdjacentHTML('beforeend', `<div class="image-container">Sorry! No Similar Recipe</div>`)
+
+                        }
+                    }, 0)
 
                 }
-            }, 0)
 
+            }
         }
-
+    } catch (e) {
+        console.log(e)
     }
 }
