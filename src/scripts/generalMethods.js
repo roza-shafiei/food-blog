13c4a1799126a56002070edb4bca834dd694f7dd
roza <roza_shafiei@yahoo.com>
2024-07-31 19:53:10 +0330

    fix: footer links and home page link
    remove index page from src and add it to the root

--- src/scripts/generalMethods.js
+++ src/scripts/generalMethods.js
@@ -50,7 +50,7 @@ function createSingleFoodCard(recipe, itemsContainer) {
                 </div>
                       <div class="food-card__info">
                              <span class="food-card__info--span">${recipe.prepTimeMinutes} Min - ${recipe.servings} Serves - ${recipe.rating} Rating</span>
-                             <a class="secondary-btn btn" href="src/recipeId.html?id=${recipe.id}">read</a>
+                             <a class="secondary-btn btn" href="recipeId.html?id=${recipe.id}">read</a>
                  </div>
             </div>
        </div>`)
